f978851c4e75d0efc5c488868f4df5f463e796d8
samuel.documet <samuel.documet@pagonxt.com>
2025-01-21 20:28:41 +0100

    core(BACK-0001): trades controller implemented ok

--- src/routes/trades.routes.ts
+++ src/routes/trades.routes.ts
@@ -1,49 +1,18 @@
-import { Router, Request, Response} from 'express';
-import { io } from '../server';
+import { Router } from 'express';
+import {
+    getAllTrades,
+    getTradeById,
+    createTrade,
+    updateTrade,
+    deleteTrade,
+} from '../controllers/trades.controller';
+
 const router = Router();
 
-/**
- * Retrieve all trades (filter by user, planet, status)
- * **/
-router.get('/', async (req: Request, res: Response)=>{
-    res.json({response: 'This is the ALL TRADES -GET- response'});
-});
-
-/**
- * Retreive a specific trade's details
- * **/
-router.get('/:id', async (req: Request, res: Response)=>{
-    const { id } = req.params;
-    res.json({response: `This is the TRADE with id=${id} -GET- details response`});
-});
-
-/**
- * Create a new trade (buy/sell/propose)
- * **/
-router.post('/', async (req: Request, res: Response)=> {
-    const newTrade = {
-        id: '0001',
-        type: 'sell',
-        planet: 'earth'
-    };
-    io.emit('tradeCreated', newTrade );
-    res.status(201).json({response: 'This is the NEW TRADE -POST- response', trade: newTrade});
-});
-
-/**
- * Updates a trade (approved|reject a proposal)
- * **/
-router.put('/:id', async (req: Request, res: Response)=>{
-    const { id } = req.params;
-    res.json({response: `This is the UPDATE TRADE -PUT- with id=${id} response`});
-});
-
-/**
- * Cancel a trade
- * **/
-router.delete('/:id', async (req: Request, res: Response)=>{
-    const { id } = req.params;
-    res.json({response: `This is the CANCEL TRADE -DELETE- with id=${id} response`});
-});
+router.get('/', getAllTrades);
+router.get('/:tradeId', getTradeById);
+router.post('/', createTrade);
+router.put('/:tradeId', updateTrade);
+router.delete('/:tradeId', deleteTrade);
 
 export default router;
