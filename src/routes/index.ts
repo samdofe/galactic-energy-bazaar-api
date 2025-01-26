ba549527fa4cb5a8c02b44930370bb8003a06f5f
samuel.documet <samuel.documet@pagonxt.com>
2025-01-26 22:21:21 +0100

    feat(BACK-0005): add i18n routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,12 +3,14 @@ import authRoutes from './auth.routes';
 import planetsRoutes from "./planets.routes";
 import tradesRoutes from "./trades.routes";
 import userRoutes from './user.routes';
+import i18nRoutes from "./i18n.routes";
 import {authenticate} from "../middleware/auth.middleware";
 
 const API_VERSION = process.env.API_VERSION || 'v1';
 const router = Router();
 
 router.use(`/${API_VERSION}/auth`, authRoutes);
+router.use(`/${API_VERSION}/i18n`, i18nRoutes);
 router.use(`/${API_VERSION}/planets`, authenticate, planetsRoutes);
 router.use(`/${API_VERSION}/trades`, authenticate, tradesRoutes);
 router.use(`/${API_VERSION}/users`, authenticate, userRoutes);
